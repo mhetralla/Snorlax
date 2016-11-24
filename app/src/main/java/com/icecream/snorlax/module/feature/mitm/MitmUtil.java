8b2c730bcca89bdae7973ae9234487a637910059
Antoine LUCAS <git.alucas@gmail.com>
2016-11-24 17:26:35 +0100

    Add log

--- app/src/main/java/com/icecream/snorlax/module/feature/mitm/MitmUtil.java
+++ app/src/main/java/com/icecream/snorlax/module/feature/mitm/MitmUtil.java
@@ -11,10 +11,16 @@ import rx.Observable;
 import rx.functions.Func1;
 
 public class MitmUtil {
+	private static final String LOG_PREFIX = "[" + MitmUtil.class.getCanonicalName() + "]";
+
 	public static Func1<MitmEnvelope, Observable<MitmMessages>> filterResponse(final RequestType... types) {
 		final List<RequestType> typeList = Arrays.asList(types);
 		return envelope -> {
 			final List<RequestOuterClass.Request> requests = envelope.getRequest().getRequestsList();
+			if (requests.size() != envelope.getResponse().getReturnsCount()) {
+				Log.d(LOG_PREFIX + " :  Not the same number of Requests and Responses");
+				return Observable.empty();
+			}
 
 			for (int i = 0; i < requests.size(); i++) {
 				final RequestType requestType = requests.get(i).getRequestType();
@@ -23,7 +29,7 @@ public class MitmUtil {
 					continue;
 				}
 
-				Log.d("[MITM Util] Packed accepte : " + requestType);
+				Log.d(LOG_PREFIX + "Accepted packet : " + requestType);
 				return Observable.just(new MitmMessages(requestType, requests.get(i).getRequestMessage(), envelope.getResponse().getReturns(i)));
 			}
 
