41e58b65068a313b4a42874b986415e3f08c56e7
Antoine Lucas <git.alucas@gmail.com>
2016-12-08 22:59:32 +0100

    Move collect defender action to build script

--- app/src/main/java/com/alucas/snorlax/module/feature/collect/CollectDefenderNotification.java
+++ app/src/main/java/com/alucas/snorlax/module/feature/collect/CollectDefenderNotification.java
@@ -10,12 +10,11 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 
+import com.alucas.snorlax.BuildConfig;
 import com.alucas.snorlax.module.context.pokemongo.PokemonGo;
 
 @Singleton
 public class CollectDefenderNotification {
-	private static final String ACTION_COLLECT_DEFENDER_BONUS = "com.alucas.snorlax.BROADCAST_COLLECT_DEFENDER_BONUS";
-
 	private final AlarmManager mAlarmManager;
 	private final PendingIntent mAlarmPendingIntent;
 
@@ -23,7 +22,7 @@ public class CollectDefenderNotification {
 	CollectDefenderNotification(@PokemonGo final Context context, final AlarmManager alarmManager) {
 		this.mAlarmManager = alarmManager;
 
-		final Intent alarmIntent = new Intent(ACTION_COLLECT_DEFENDER_BONUS).addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+		final Intent alarmIntent = new Intent(BuildConfig.INTENT_COLLECT_DEFENDER).addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
 		this.mAlarmPendingIntent = PendingIntent.getBroadcast(context, 1, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 	}
 
